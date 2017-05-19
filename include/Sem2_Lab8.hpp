7c618c436450f41a4d90e9cab733ff7fe2c746b9
LeakySum <leakysum60@gmail.com>
2017-05-19 10:37:52 +0300

    Update Sem2_Lab8.hpp

--- include/Sem2_Lab8.hpp
+++ include/Sem2_Lab8.hpp
@@ -1,4 +1,5 @@
 
+#include "stdafx.h"
 #include <iostream>
 
 using namespace std;
@@ -15,13 +16,13 @@ class Complex {
 
 	Complex& sum(const Complex& number)
 	{
-		Complex buf(x + number.give_x(), y + number.give_y());
+		Complex buf(x + number.x, y + number.y);
 		return buf;
 	}
 
 	Complex& sub(const Complex& number)
 	{
-		Complex buf(x - number.give_x(), y - number.give_y());
+		Complex buf(x - numberx, y - number.y);
 		return buf;
 	}
 	Complex& sub(T number)
@@ -39,7 +40,7 @@ class Complex {
 	Complex& div(int number)
 	{
 		if (number) {
-			Complex buf(T(x / number), T(y / number));
+			Complex buf(T(x /number), T(y /number));
 			return buf;
 		}
 		else
@@ -50,14 +51,7 @@ class Complex {
 		}
 	}
 public:
-	T give_x() const
-	{
-		return x;
-	}
-	T give_y() const
-	{
-		return y;
-	}
+	
 	Complex()
 	{
 		x = 0.0;
@@ -68,14 +62,14 @@ public:
 		this->x = x;
 		this->y = y;
 	}
-
+	
 
 	Complex& mul_complex(const Complex& number)
 	{
-		Complex buf(x*number.give_x() - y*number.give_y(), x*number.give_y() + y*number.give_x());
+		Complex buf(x*number.x - y*number.y, x*number.y + y*number.y);
 		return buf;
 	}
-
+	
 	Complex operator+(const Complex& comp)
 	{
 		return sum(comp);
@@ -106,11 +100,11 @@ public:
 	}
 	Complex operator=(const Complex& comp)
 	{
-		x = comp.give_x();
-		y = comp.give_y();
+		x = comp.x;
+		y = comp.y;
 		return *this;
 	}
-	Complex operator+=(T num)
+		Complex operator+=(T num)
 	{
 		*this = *this + num;
 		return *this;
@@ -147,26 +141,26 @@ public:
 	}
 	bool operator==(const Complex& comp)
 	{
-		if ((x == comp.give_x()) && (y == comp.give_y()))
+		if ((x == comp.x) && (y == comp.y))
 			return 1;
 		else
 			return 0;
 	}
-	
+	template <class T>
 	friend ostream& operator<<(ostream&, const Complex<T>&);
-	
+	template <class T>
 	friend istream& operator >> (istream&, Complex<T>&);
 };
 
 template <class T>
 ostream& operator<<(ostream& out, const Complex<T>& num)
 {
-	if (num.give_x() && num.give_y())
-		out << num.give_x() << " + " << num.give_y() << 'i' << endl;
-	else if (num.give_x() && !num.give_y())
-		out << num.give_x() << endl;
-	else if (!num.give_x() && num.give_y())
-		out << num.give_y() << 'i' << endl;
+	if (num.x && num.y)
+		out << num.x << " + " << num.y << 'i' << endl;
+	else if (num.x && !num.y)
+		out << num.x << endl;
+	else if (!num.x && num.y)
+		out << num.y << 'i' << endl;
 	else
 		out << 0 << endl;
 	return out;
@@ -181,4 +175,32 @@ istream& operator >> (istream &in, Complex<T>& number)
 	Complex<T> buf(x, y);
 	number = buf;
 	return in;
+}
+
+int main() {
+	Complex<double> num1(3, 1);
+	Complex<double> num2(2, 2);
+		cout << "num1 output\n" << num1 << endl;
+		cout << "num2 output\n" << num2 << endl;
+		cout << "num1 += num2\n" << (num1 += num2) << endl;
+		cout << "num1 *= num2\n" << (num1 *= num2) << endl;
+		cout << "num1 *= 2\n" << (num1 *= 2) << endl;
+		cout << "num1 /= 10\n" << (num1 /= 10) << endl;
+	
+	cout << "num1 == num1" << endl;
+	if (num1 == num1)
+		cout << "works" << endl;
+	
+	cout << "testnig >> " << endl;
+	Complex<double> num(10, 25);
+	cout << "Print number" << endl;
+	cout << num;
+	cout << "Enter number" << endl;
+	cin >> num;
+	cout << "Print number" << endl;
+	cout << num;
+	system("pause");
+
+
+	return 0;
 }
